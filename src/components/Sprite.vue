393e067bf777026232716cb3d4756e652b66ac06
Maija <hello@maijagrudule.com>
2020-09-15 01:15:30 +0200

    Added  sprite animation block for signals

---
+++ src/components/Sprite.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="border border-black-900 sprite-wrapper-outer">
+    <div class="sprite-wrapper position-relative">
+      <div class="sprite-content" :style="sprites">
+        <g-image src="../assets/img/sprites/feel.jpg" height="480" width="640" class="transparent"></g-image>
+      </div>
+    </div>
+    <div class="p-3">
+      <h3 v-if="title">{{title}}</h3>
+      <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi exercitationem quod deleniti earum dolorem nesciunt expedita officia laboriosam accusamus laudantium, eius iusto fugit qui quae molestiae magnam non vero dolor.</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      required: false,
+    },
+    content: { required: false },
+    img: {
+      required: false,
+    },
+    frames: {
+      required: false,
+      default: 3,
+    },
+  },
+  computed: {
+    setImage: function () {
+      return require("@/assets/img/sprites/" + this.img);
+    },
+    sprites() {
+      return `background: url("assets/img/${this.img}") left center; background-size: auto 100%;
+`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.sprite-wrapper-outer {
+  max-width: 640px;
+}
+.sprite-wrapper {
+  display: inline-block;
+  max-width: 640px;
+  max-height: 480px;
+  overflow: hidden;
+}
+.sprite-content {
+  width: auto;
+  height: 100%;
+  &:hover {
+    animation: play 0.5s steps(3) infinite;
+  }
+}
+.transparent {
+  opacity: 0.3;
+}
+@keyframes play {
+  100% {
+    background-position: -300%;
+  }
+}
+</style>
