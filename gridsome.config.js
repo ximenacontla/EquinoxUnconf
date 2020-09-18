710f35c0a66d754a43f6f6fbd9d33340d81ec88d
Maija <hello@maijagrudule.com>
2020-09-18 22:50:48 +0200

    updated configs site Url

--- gridsome.config.js
+++ gridsome.config.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require(
 module.exports = {
   siteName: 'Equinox Unconference | 22 Sep 2020 ',
   siteDescription: "An open space to teach, learn and share",
-  siteUrl: "https://equinox.hackalong.io/",
+  siteUrl: "https://unconference.hackalong.io/",
   templates: {
     Tag: [{
       path: '/event/tag/:title',
